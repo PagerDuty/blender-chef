f59bfcc9b54fed6783bb6196ebbaaf603ddf7258
Ranjib Dey <ranjib@pagerduty.com>
2015-01-02 01:08:32 -0800

    run specs against chef zero instead of mocking

--- spec/blender/discoveries/chef_spec.rb
+++ spec/blender/discoveries/chef_spec.rb
@@ -4,36 +4,15 @@ require 'blender/discoveries/chef'
 describe Blender::Discovery::Chef do
   let(:discovery){described_class.new}
   it '#search' do
-    query = double(Chef::Search::Query)
-    expect(query).to receive(:search).with(
-      :node,
-      '*:*',
-      filter_result: {
-        attribute: ['fqdn']
-      }
-    ).and_return([['data'=> { 'attribute'=> 'a'}]])
-    expect(Chef::Search::Query).to receive(:new).and_return(query)
-    expect(discovery.search).to eq(['a'])
+    expect(discovery.search.size).to be(10)
   end
   it '#search with options' do
     disco = described_class.new(
-      config_file: 'foo.rb',
-      node_name: 'bar',
-      client_key: 'baz.rb',
-      attribute: 'x.y.z'
+      chef_server_url: 'http://localhost:8889',
+      node_name: 'admin',
+      client_key: SpecHelper.client_key.path,
+      attribute: 'name'
     )
-    query = double(Chef::Search::Query)
-    expect(Chef::Config).to receive(:from_file).with('foo.rb')
-    expect(query).to receive(:search).with(
-      :node,
-      'name:x',
-      filter_result: {
-        attribute: %w{x y z}
-      }
-    ).and_return([[{'data'=>{'attribute'=> 123}}]])
-    expect(Chef::Search::Query).to receive(:new).and_return(query)
-    expect(disco.search('name:x')).to eq([123])
-    expect(Chef::Config[:client_key]).to eq('baz.rb')
-    expect(Chef::Config[:node_name]).to eq('bar')
+    expect(disco.search('name:node-1')).to eq(['node-1'])
   end
 end
